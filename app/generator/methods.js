b0a491a7c04d0316afbec8209a930b50cca27ee4
Alex Lambrinos <alex.lambrinos@gmail.com>
2014-04-17 22:46:14 +1000

    Implemented sub resource uri's and method parameters

--- app/generator/methods.js
+++ app/generator/methods.js
@@ -14,69 +14,102 @@ var indentAmount = 2;
 module.exports = {};
 
 /**
- * Returns the method function parameters for the provided HTTP verb
- *
- * @param {String} method - The HTTP verb
- * @returns {string}
- */
-var getQueryParametersForMethod = function(method) {
-  switch (method) {
-    case 'query':
-      return 'query';
-    case 'get':
-      return 'id';
-    case 'post':
-      return 'entity';
-    case 'put':
-      return 'entity';
-    case 'delete':
-      return 'id';
-  }
-
-  return '';
-};
-
-/**
  * Returns the api function parameters for the provided HTTP verb
  *
- * @param {String} method - The HTTP verb
- * @returns {string}
+ * @param {string} method - The HTTP verb
+ * @param {string} relativeUri - The uri to set as the first parameter.
+ * @returns {Object}
  */
-var getApiQueryParametersForMethod = function(method) {
+var getParametersForMethod = function(method, relativeUri) {
+
+  var result = {
+    method: '',
+    api: '',
+    rawMethod: [],
+    rawApi: []
+  };
+
+  // Remove the last '/{id}' instance from the uri
+  var uri = relativeUri.replace(/\/{id}$/i, '');
+  var segments = uri.split('/');
+  var uriSegments = [];
+  var apiParams = [];
+  var params = [];
+
+  // exclude the first and last 2 entries
+  segments = segments.splice(1, segments.length);
+
+  segments.forEach(function(segment, index) {
+
+    if (segment == '{id}') {
+      // If the segment is an id parameter then retrieve it from the list of params.
+      // the value will always exist because of the way the uri is constructed.
+      uriSegments.push('/\' + ' + params[params.length - 1] + ' + \'');
+    } else {
+      // If the segment is not an if parameter then just add it to the uri segments
+      uriSegments.push('/' + segment);
+
+      // Only add an id parameter if it's not the last resource in the uri.
+      if (index < segments.length - 2) {
+        params.push(inflect.singularize(segment) + 'Id');
+      }
+    }
+  });
+
+  apiParams.push('\'' + uriSegments.join('') + '\'');
+
+  // Add additional parameters
   switch (method) {
     case 'query':
-      return 'resourceName, null, query';
+      params.push('query');
+      apiParams.push('null', 'query');
+      break;
     case 'get':
-      return 'resourceName, id';
+      params.push('id');
+      apiParams.push('id');
+      break;
     case 'post':
-      return 'resourceName, entity';
+      params.push('entity');
+      apiParams.push('entity');
+      break;
     case 'put':
-      return 'resourceName, entity.id, entity';
+      params.push('entity');
+      apiParams.push('entity.id', 'entity');
+      break;
     case 'delete':
-      return 'resourceName, id';
+      params.push('id');
+      apiParams.push('id');
+      break;
   }
 
-  return '';
+  result.rawMethod = params;
+  result.rawApi = apiParams;
+  result.method = params.join(', ');
+  result.api = apiParams.join(', ');
+
+  return result;
 };
 
 /**
  * A helper method for generating RAML resource methods. eg. GET, POST, PUT and DELETE
  *
- * @param {Object} ramlResource The RAML resource object to generate the methods for
- * @returns {String} The compiled template string
+ * @param {Object} ramlResource - The RAML resource object to generate the methods for
+ * @param {string} relativeUri - The uri of the resource
+ * @returns {string} The compiled template string
  */
-var generateMethods = function(ramlResource) {
+var generateMethods = function(ramlResource, relativeUri) {
   var isCollection = ramlResource.type.toLowerCase() == 'collection';
   var methodData = [];
 
   ramlResource.methods.forEach(function(item, index) {
     var transformedMethodName = isCollection && item.method == 'get' ? 'query' : item.method;
+    var parameters = getParametersForMethod(transformedMethodName, relativeUri);
     methodData.push({
       description: documentation.formatDescription(item.description, true),
       factoryMethodName: generatorUtil.toCamelCase(item.displayName || transformedMethodName),
       name: transformedMethodName,
-      queryParameters: getQueryParametersForMethod(transformedMethodName),
-      apiQueryParameters: getApiQueryParametersForMethod(transformedMethodName),
+      queryParameters: parameters.method,
+      apiQueryParameters: parameters.api,
       separator: index != ramlResource.methods.length - 1 ? ',' : ''
     });
   });
@@ -88,12 +121,19 @@ var generateMethods = function(ramlResource) {
   return generatorUtil.indentText(indentAmount, compiledMethod);
 };
 
-var generateSubResource = function(ramlResource) {
+/**
+ * A helper method for generating a resource within a resource
+ *
+ * @param ramlResource - The RAML resource to generate the sub resource for
+ * @param relativeUri The relative uri of the resource
+ * @returns {string} The compiled sub resource
+ */
+var generateSubResource = function(ramlResource, relativeUri) {
   return _.template(subResourceTemplateText, {
     resource: {
       name: ramlResource.relativeUri.replace('/', ''),
       description: documentation.formatDescription(ramlResource.description, true),
-      methods: generateMethods(ramlResource)
+      methods: generateMethods(ramlResource, relativeUri)
     }
   });
 };
@@ -103,27 +143,29 @@ var generateSubResource = function(ramlResource) {
  *
  * @param {Object} ramlResource - The RAML resource to generate methods and sub resources for.
  * @param {Number} level - The depth of recursion mainly used for indentation.
- * @returns {String} The compiled template string.
+ * @param {string} relativeUri - The uri of the resource
+ * @returns {string} The compiled template string.
  */
-var recursResources = function(ramlResource, level) {
+var recursResources = function(ramlResource, level, relativeUri) {
   var compiledResource = "";
+  var uri = relativeUri + ramlResource.relativeUri;
   if (level != 0) {
     if (ramlResource.type == 'collection') {
-      compiledResource += generateSubResource(ramlResource);
+      compiledResource += generateSubResource(ramlResource, uri);
     } else {
-      compiledResource += generateMethods(ramlResource);
+      compiledResource += generateMethods(ramlResource, uri);
     }
 
     compiledResource = generatorUtil.indentText(indentAmount * level, compiledResource);
   } else {
-    compiledResource = generateMethods(ramlResource);
+    compiledResource = generateMethods(ramlResource, uri);
   }
 
   if (ramlResource.resources && ramlResource.resources.length > 0) {
     ramlResource.resources.forEach(function(resource) {
       var incrementer = resource.type == 'collection' ? 1 : 0;
       compiledResource = compiledResource.trimRight() + ',' + endOfLine;
-      compiledResource += recursResources(resource, level + incrementer);
+      compiledResource += recursResources(resource, level + incrementer, uri);
 
       if (resource.type == 'collection') {
         compiledResource = compiledResource.trimRight() + endOfLine;
@@ -139,13 +181,13 @@ var recursResources = function(ramlResource, level) {
  * Generates the service methods for the provided RAML resource object
  *
  * @param {Object} ramlResource - The RAML resource to generate the methods and sub resources for.
- * @returns {String} The compiled template string
+ * @returns {string} The compiled template string
  */
 module.exports.generate = function(ramlResource) {
   // Get all of the required templates
   methodTemplateText = generatorUtil.readFileAsString(methodTemplatePath);
   subResourceTemplateText = generatorUtil.readFileAsString(subResourceTemplatePath);
-  var compiledTemplate = recursResources(ramlResource, 0);
+  var compiledTemplate = recursResources(ramlResource, 0, '');
   compiledTemplate = generatorUtil.indentText(indentAmount, compiledTemplate);
   return compiledTemplate;
 };
--- app/templates/service.js
+++ app/templates/service.js
@@ -2,9 +2,6 @@ angular.module('<%= app.name %>')
 
 <%= resource.description %>
 .factory('<%= resource.displayName %>', ['Api', function(Api) {
-  var resourceName = '<%= resource.relativeUri %>';
-  var expand = 'expand(<%= resource.expand %>)';
-
   return {
 <%= resource.methods %>
   };
